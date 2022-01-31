7fbbbf9f73f0626420608ebd36d5b309b8d6ad89
wlb-silva <windsor_lima@hotmail.com>
2022-01-31 12:58:42 -0300

    Inseri sysout

--- CursoItb/src/Exercicios1Sem/Aula05_Ex02.java
+++ CursoItb/src/Exercicios1Sem/Aula05_Ex02.java
@@ -1,10 +1,22 @@
 package Exercicios1Sem;
 
+import javax.swing.JOptionPane;
+
 public class Aula05_Ex02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//trabalhando com JOptionPane//
+		
+		String aux="";
+		double base, altura=0;
+		
+		base=Integer.parseInt(JOptionPane.showInputDialog("Entre com o valor da base"));
+		altura=Integer.parseInt(JOptionPane.showInputDialog("Entre com o valor da altura"));
+		double area= base*altura/2;
+		
+		JOptionPane.showMessageDialog(null,"Área do Triângulo: "+ area);
+		
 	}
 
 }
