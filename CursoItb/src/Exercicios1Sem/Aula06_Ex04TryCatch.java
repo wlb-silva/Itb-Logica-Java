cf9ad30d53a78ded78402587b801fee0ad6b30bb
wlb-silva <windsor_lima@hotmail.com>
2022-02-01 11:35:55 -0300

    trycatch - piscina

---
+++ CursoItb/src/Exercicios1Sem/Aula06_Ex04TryCatch.java
@@ -0,0 +1,23 @@
+package Exercicios1Sem;
+
+import javax.swing.JOptionPane;
+
+public class Aula06_Ex04TryCatch {
+
+	public static void main(String[] args) {
+
+		double metroCubico, total, comprimento, largura, profundidade=0;
+		try {
+		comprimento=Double.parseDouble(JOptionPane.showInputDialog("Informe o comprimento da piscina: "));
+		largura=Double.parseDouble(JOptionPane.showInputDialog("Informe a largura da piscina: "));
+		profundidade=Double.parseDouble(JOptionPane.showInputDialog("Informe a profundidade da piscina: "));
+		metroCubico=comprimento*largura*profundidade;
+		total=metroCubico*45;
+		JOptionPane.showMessageDialog(null, "A piscina solicitada terá "+metroCubico
+				+" metros cúbicos.\n"+"O valor da construção será de: R$ "+total);
+	}catch (Exception e) {
+		JOptionPane.showMessageDialog(null, "Informe apenas números!");
+	}
+
+}
+}
