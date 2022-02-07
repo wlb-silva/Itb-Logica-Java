5b30945a2600a69370cb79b1d93b2f08976bc791
wlb-silva <windsor_lima@hotmail.com>
2022-02-07 20:48:44 -0300

    laco for exemplo 2

---
+++ CursoItb/src/Exercicios1Sem/Aula08_Exemplo2LacoFor.java
@@ -0,0 +1,15 @@
+package Exercicios1Sem;
+
+import javax.swing.JOptionPane;
+
+public class Aula08_Exemplo2LacoFor {
+
+	public static void main(String[] args) {
+		
+		for(int cont=1;cont<=10;cont++) {
+			JOptionPane.showMessageDialog(null, "Java- "+ cont+ " ");
+		}
+				
+	}
+
+}
