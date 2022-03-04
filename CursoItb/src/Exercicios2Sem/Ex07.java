dc4bdbaa663e9bf70d777e8d939c0a4149d819fe
wlb-silva <windsor_lima@hotmail.com>
2022-03-04 11:11:25 -0300

    fiz comentario no prog

--- CursoItb/src/Exercicios2Sem/Ex07.java
+++ CursoItb/src/Exercicios2Sem/Ex07.java
@@ -6,17 +6,9 @@ public class Ex07 {
 
 	public static void main(String[] args) {
 
+		//o programa deve ler e somar números até o usuário digitar 0//
 		double numero=0, soma=0;
 		boolean continuar=true;
-		
-		/*for(int i=1;i<=10;i++) {
-			numero=Double.parseDouble(JOptionPane.showInputDialog("Informe um valor"));
-			if (numero==0) {
-				System.exit(0);	
-			} else {
-			soma+=numero;
-			}
-		}JOptionPane.showMessageDialog(null, soma);*/
 		
 		int i=0;
 		while (continuar) {
